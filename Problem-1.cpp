0a075435c45bdcf2471be3468d1c38cb565a376d
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-07-06 18:17:28 -0400

    Completed problem-1

---
+++ Problem-1.cpp
@@ -0,0 +1,15 @@
+//taught in lecture
+//nlogk, quite some savings compared to usual nlogn
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int num : nums) {
+            pq.push(num);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
